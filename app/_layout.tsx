6838215df8f263ed66285859fce6bd00549f8392
entiendoNull <jacobnclausen@gmail.com>
2025-02-22 09:56:45 +0100

    Removed custom tab slot renderer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,19 +2,14 @@ import * as React from "react";
 import { StyleSheet } from "react-native";
 import { Tabs, TabList, TabTrigger, TabSlot } from "expo-router/ui";
 import { CustomTabList } from "@/components/CustomTabList";
-import { CustomTabSlotRenderer } from "@/components/CustomTabSlotRenderer";
 
 export default function Layout() {
   return (
     <Tabs>
-      <TabSlot
-        renderFn={(descriptor, options) => (
-          <CustomTabSlotRenderer descriptor={descriptor} options={options} />
-        )}
-      />
+      <TabSlot />
       <CustomTabList />
       <TabList style={styles.tabList}>
-        <TabTrigger name="home" href="/" />
+        <TabTrigger name="home" href="/(home)" />
         <TabTrigger name="search" href="/search" />
         <TabTrigger name="settings" href="/settings" />
       </TabList>
